075041fb07c1c03a1f8f7b95ad4f7ddf83805501
AncrypeR <48804515+Corruptcodesu@users.noreply.github.com>
2019-06-08 23:52:19 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt-get install figlet
+figlet installer
+
+#move
+cd /data/data/com.termux/files/home/msfshit
+
+cp msfshit /data/data/com.termux/files/usr/bin
+
+#make exec
+chmod +x /data/data/com.termux/files/usr/bin/msfshit
+
+to be continued
